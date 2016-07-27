0c095746eeb6d93a24711ea2179b22433d39d202
valentinewilson <valentinewilson@10.227.157.189>
2016-07-27 11:11:44 -0400

    - put in leors optimization

--- testproject/test.py
+++ testproject/test.py
@@ -2,14 +2,36 @@ import tensorflow as tf
 
 import numpy as np
 from numpy import count_nonzero
+from tensorflow.contrib.metrics.python import metrics
 
 # Data sets
+#returns False if the index is outside the array bounds or if the array value at that index isn’t searchterm
+def arraysearch(a, searchterm, sindex):
+    if sindex>=len(a):
+        return False
+    elif a[sindex]==searchterm:
+        return True
+    else:
+        return False
+#returns the lexicographically next term in the sequence of possible nodes and layers
+def testincrements(a, start, end, increment):
+    c=0
+    while arraysearch(a, end, c) and c<len(a):
+        a[c]=start
+        c+=1
+    if c==len(a):
+        a.append(start)
+        return a
+    else:
+        a[c]=a[c]+increment
+        return a
+
 IRIS_TRAINING = "tfidfvectorsnew1.csv"
 IRIS_TEST = "tfidfvectorsnew2.csv"
 
 # Load datasets.
-training_set = tf.contrib.learn.datasets.base.load_csv(filename=IRIS_TRAINING, target_dtype=np.int64)
-test_set = tf.contrib.learn.datasets.base.load_csv(filename=IRIS_TEST, target_dtype=np.int64)
+training_set = tf.contrib.learn.datasets.base.load_csv(filename=IRIS_TRAINING, target_dtype=np.int64, has_header=False)
+test_set = tf.contrib.learn.datasets.base.load_csv(filename=IRIS_TEST, target_dtype=np.int64, has_header=False)
 
 x_train, x_test, y_train, y_test = training_set.data, test_set.data, \
   training_set.target, test_set.target
@@ -25,45 +47,61 @@ for a in y_test:
 print(pos)
 avgauc=0
 avgf1=0
-ba=10
-# Build 3 layer DNN with 10, 20, 10 units respectively.
-for i in range(ba):
-    classifier = tf.contrib.learn.DNNClassifier(hidden_units=[20,20,20,50, 50,50,50, 100], n_classes=2)
+ba=20
+optimizerarraystart=[20, 20, 20]
+optimizerarraycurrent=[20, 20, 20];
+maxoptimizerarray=optimizerarraystart;
+maxavgf1=0
+# Build 3 layer DNN with current array of layers and nodes, set <x to 6 on server.
+while len(optimizerarraycurrent)<6:
+    for i in range(ba):
+        classifier = tf.contrib.learn.DNNClassifier(hidden_units=optimizerarraycurrent, n_classes=2)
+   
 
-    # Fit model.
-    classifier.fit(x=x_train, y=y_train, steps=1000)
-    y = classifier.predict(x_test)
-    print(count_nonzero(y))
-    print (str(y))
-    print(y_test)
-    tp=0
-    tn=0
-    fn=0
-    fp=0
-    for c in range(len(y_test)):
-        if y_test[c]=='1' and y[c]==1:
-            tp+=1
-        if y_test[c]=='0' and y[c]==1:
-            fp+=1
-        if y_test[c]=='1' and y[c]==0:
-            fn+=1
-        if y_test[c]=='0' and y[c]==0:
-            tn+=1
-    # Evaluate accuracy.
-    if tp==0:
-        precision=0
-        recall=0
-        f1=0
-    else:
-        precision= tp/(tp+fp)
-        recall=tp/(tp+fn)
-        f1=2*(precision*recall)/(precision+recall)
-    avgf1+=f1
-    accuracy_score = classifier.evaluate(x=x_test, y=y_test)['eval_auc']
-    avgauc+=accuracy_score
-    print("Accuracy: "+str(accuracy_score))
-    print("F1: "+str(f1))
-avgauc=avgauc/ba
-avgf1=avgf1/ba
-print("Average auc: "+ str(avgauc))
-print("Average f1: "+str(avgf1))
+        # Fit model.
+        classifier.fit(x=x_train, y=y_train, steps=1000)
+        y = classifier.predict(x_test)
+        print(count_nonzero(y))
+        print (str(y))
+        print(y_test)
+        tp=float(0.0)
+        tn=float(0.0)
+        fn=float(0.0)
+        fp=float(0.0)
+        for c in range(len(y_test)):
+            if y_test[c]=='1' and y[c]==1:
+                tp+=1
+            if y_test[c]=='0' and y[c]==1:
+                fp+=1
+            if y_test[c]=='1' and y[c]==0:
+                fn+=1
+            if y_test[c]=='0' and y[c]==0:
+                tn+=1
+        # Evaluate accuracy.
+        if tp+fp==0 or tp+fn==0:
+            precision=0
+            recall=0
+            f1=0
+        else:
+            precision= tp/(tp+fp)
+            recall=tp/(tp+fn)
+            f1=2*(precision*recall)/(precision+recall)
+        avgf1+=f1
+        accuracy_score = classifier.evaluate(x=x_test, y=y_test)['eval_auc']
+        avgauc+=accuracy_score
+        print("Accuracy: "+str(accuracy_score))
+        print("F1: "+str(f1))
+        print("Precision : " + str(precision))
+        print("Recall : " + str(recall))
+    avgauc=avgauc/ba
+    avgf1=avgf1/ba
+    print(metrics);
+    print("Average auc: "+ str(avgauc))
+    print("Average f1: "+str(avgf1))
+    if avgf1>maxavgf1:
+        maxoptimizerarray=optimizerarraycurrent
+        maxavgf1=avgf1
+        #change these values as needed(20, 50, 15) are suggested for last 3 params
+    optimizerarraycurrent=testincrements(optimizerarraycurrent, 20, 50, 15) 
+print(str(maxoptimizerarray))
+print(str(maxavgf1))
